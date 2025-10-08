49ded6a06c0d7c2b95f55850edf6ea45e329339b
tabcat <tabcat00@proton.me>
2025-10-08 18:13:13 -0500

    feat: fileListSource
    Support FileList sources

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export type { FileCandidate } from "ipfs-unixfs-importer";
+export { fileListSource } from "./file-list.js";
 export { fsEntrySource } from "./fs-entry.js";
 export { fsHandleSource } from "./fs-handle.js";
 export type {
